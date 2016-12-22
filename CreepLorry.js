33a42d61c07a2eeba116d36a8a30fa377bd3924d
ofirl <ofirl24@gamil.com>
2016-12-22 16:42:04 +0200

    better enegy harvesting system - added files

---
+++ CreepLorry.js
@@ -0,0 +1,88 @@
+var Constants = require('Constants');
+var Cache = require('Cache');
+var CONSTANTS = {
+    PARALLEL_WALL_REPAIR : 3,
+};
+var ACTIONS = {
+    DEPOSIT : 1,
+    WITHDRAW : 2,
+};
+
+function creepLorry(creep, depositManager, resourceManager) {
+    this.creep = creep;
+    this.depositManager = depositManager;
+    this.resourceManager = resourceManager;
+};
+
+creepLorry.prototype.init = function() {
+    if (!this.remember('source-container'))
+        this.remember('source-container', this.depositManager.getLonelyContainer(this.resourceManager.population.creeps, 'CreepLorry').id);
+    
+    if (!this.remember('repair-wall'))
+        this.remember('repair-wall', 0);
+
+    if(!this.remember('srcRoom')) {
+        this.remember('srcRoom', this.creep.room.name);
+    }
+    if (!this.remember('last-action'))
+        this.remember('last-action', ACTIONS.WITHDRAW);
+    if(this.moveToNewRoom() == true) {
+        return;
+    }
+
+    //if(this.randomMovement() == false) {
+    this.act();
+    //}
+};
+
+creepLorry.prototype.act = function() {
+    // creep carry is full
+    if (this.remember('last-action') == ACTIONS.WITHDRAW && this.creep.carry.energy == this.creep.carryCapacity)
+        this.remember('last-action', ACTIONS.DEPOSIT);
+
+    // creep ran out of energy
+    if (this.remember('last-action') == ACTIONS.DEPOSIT && this.creep.carry.energy == 0)
+        this.remember('last-action', ACTIONS.WITHDRAW);
+
+    // creep should withdraw energy
+    if (this.remember('last-action') == ACTIONS.WITHDRAW) {
+        var droppedEnergy = this.creep.pos.findClosestByPath(this.depositManager.droppedEnergy);
+        droppedEnergy = null;
+        if (droppedEnergy != null) {
+            if (this.creep.pickup(droppedEnergy) == ERR_NOT_IN_RANGE)
+                this.creep.moveTo(droppedEnergy);
+        }
+        else {
+            var source = Game.getObjectById(this.remember('source-container'));
+            //var source = this.creep.pos.findClosestByPath(this.depositManager.getResourceContainers());
+            if (source && source.transfer(this.creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+                this.creep.moveTo(source);
+        }
+    }
+    // creep should deposit
+    else {
+        var target = this.depositManager.storage;
+        // storage is built
+        if (target) {
+            if (this.creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+                this.creep.moveTo(target);
+        }
+        // storage did not built yet
+        else {
+            var storage = this.creep.pos.findClosestByPath(this.depositManager.extensions.concat(this.depositManager.spawns), {filter: minerStructureFilter});
+            if (storage != undefined) {
+                if (this.creep.transfer(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+                    this.creep.moveTo(storage);
+            }
+            else {
+                storage = this.creep.pos.findClosestByPath(this.depositManager.getAvailableContainersToDeposit());
+                if (storage != undefined) {
+                    if (this.creep.transfer(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+                        this.creep.moveTo(storage);
+                }
+            }
+        }
+    }
+};
+
+module.exports = creepLorry;
---
+++ SpawnFunctions.js
@@ -0,0 +1,104 @@
+var Constants= require('Constants');
+
+module.exports = function() {
+
+    StructureSpawn.prototype.createCustomCreep =
+        function(energy, roleName) {
+            switch (roleName) {
+                case Constants.ROLE_MINER :
+                    this.spawnMiner(energy);
+                    break;
+                case Constants.ROLE_LORRY :
+                    this.spawnLorry(energy);
+                    break;
+                case Constants.ROLE_CARRIER :
+                    this.spawnCarrier(energy);
+                    break;
+                case Constants.ROLE_BUILDER :
+                    this.spawnBuilder(energy);
+                    break;
+            }
+        };
+
+    StructureSpawn.prototype.spawnMiner =
+        function(energy) {
+            var workParts = Math.min(Math.floor((energy - 150) / 100), Constants.MAX_WORK_PARTS);
+            if (workParts == 0)
+                return ERR_NOT_ENOUGH_ENERGY;
+
+            var body = [];
+            var roleName = Constants.ROLE_MINER;
+
+            for (var i = 0; i < workParts; i++)
+                body.push(WORK);
+            body.push(CARRY);
+            body.push(MOVE);
+            body.push(MOVE);
+
+            logSpawn(this, body, roleName);
+        };
+
+    StructureSpawn.prototype.spawnLorry =
+        function(energy) {
+            var carryParts = Math.min(Math.floor((energy - 50) / 100), Constants.MAX_CARRY_PARTS);
+            if (carryParts == 0)
+                return ERR_NOT_ENOUGH_ENERGY;
+            var body = [];
+            var roleName = Constants.ROLE_LORRY;
+
+            for (var i = 0; i < carryParts; i++)
+                body.push(CARRY);
+            body.push(MOVE);
+
+            logSpawn(this, body, roleName);
+        };
+
+    StructureSpawn.prototype.spawnCarrier =
+        function(energy) {
+            energy = Math.min(Constants.maxCreepCost, energy);
+            var numberOfParts = Math.floor(energy/200);
+            if (numberOfParts == 0)
+                return ERR_NOT_ENOUGH_ENERGY;
+
+            var body = [];
+            var roleName = Constants.ROLE_CARRIER;
+
+            for (let i = 0; i < numberOfParts; i++)
+                body.push(WORK);
+            for (let i = 0; i < numberOfParts; i++)
+                body.push(CARRY);
+            for (let i = 0; i < numberOfParts; i++)
+                body.push(MOVE);
+
+            logSpawn(this, body, roleName);
+        };
+
+    StructureSpawn.prototype.spawnBuilder =
+        function(energy) {
+            energy = Math.min(Constants.maxCreepCost, energy);
+            var numberOfParts = Math.floor(energy/200);
+            if (numberOfParts == 0)
+                return ERR_NOT_ENOUGH_ENERGY;
+            
+            var body = [];
+            var roleName = Constants.ROLE_BUILDER;
+
+            for (let i = 0; i < numberOfParts; i++)
+                body.push(WORK);
+            for (let i = 0; i < numberOfParts; i++)
+                body.push(CARRY);
+            for (let i = 0; i < numberOfParts; i++)
+                body.push(MOVE);
+
+            logSpawn(this, body, roleName);
+        }
+};
+
+// private
+function logSpawn(spawn, body, roleName) {
+    var name = spawn.createCreep(body, roleName + " " + Math.floor(Math.random() * 100), {role: roleName})
+    if (!(name < 0))
+        console.log('Happy Birthday ' + name);
+
+    return name;
+}
