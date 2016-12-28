12e213e139e7ad59595638848a55439866e1cae4
ofirl <ofirl24@gamil.com>
2016-12-28 18:01:36 +0200

    live debugging

--- RoomManager.js
+++ RoomManager.js
@@ -141,6 +141,8 @@ roomManager.prototype.populate = function() {
         //this.askForReinforcements()
     }
 
+    console.log(this.population.typeDistribution.CreepMiner.nextDeath);
+    
     for (var i = 0; i < this.depositManager.spawns.length; i++) {
         if (this.depositManager.spawns[i].spawning)
             continue;
